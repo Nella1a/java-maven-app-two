a2c1e42f3adc99699e69f6bfbb84e23c50bfbb79
Nella1a <m.allen@gmx.at>
2025-06-21 15:45:51 +0200

    add mising brackets to  withCredentials() funciton

--- script.groovy
+++ script.groovy
@@ -7,9 +7,12 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')])
-    sh 'docker build -t kanjamn/demo-app:jma-2.0.0 .'
-    sh 'echo $PASS | docker login -u $USER --password-stdin'
-    sh 'docker push kanjamn/demo-app:jma-2.0.0'
+            {
+                sh 'docker build -t kanjamn/demo-app:jma-2.0.0 .'
+                sh 'echo $PASS | docker login -u $USER --password-stdin'
+                sh 'docker push kanjamn/demo-app:jma-2.0.0'
+            }
+
 }
 
 
